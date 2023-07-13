2b6edc631b920be3c840a5e0a866c4ff0c266729
Shivkumar Gaded <gadedshiv@gmail.com>
2023-07-13 17:13:34 +0530

    ML Rest API

---
+++ main.py
@@ -0,0 +1,15 @@
+from flask import Flask, jsonify, request
+  
+# creating a Flask app
+app = Flask(__name__)
+
+@app.route('/', methods = ['POST'])
+def home():
+    given_name = request.json['given_name']
+    surname = request.json['surname']
+    result = '{cutomers : [{"given_name": "Michael","surname": "Jack son","street_number": "MG Road","address_1": "","address_2": "Pune","suburb": "","postcode": 411045,"state": "MH","mobile": "9756656656","email": "abc@gmail.com","account": "6545646546564","IBAN": "HDFC566565","confidence":0.99765},{"given_name": "Michael","surname": "Jack son","street_number": "","address_1": "","address_2": "","suburb": "","postcode": 411045,"state": "MH","mobile": "9756656656","email": "xyz@gmail.com","account": "56445645646","IBAN": "HDFC26155","confidence":0.98765}]}'
+    return jsonify(result)
+
+if __name__ == '__main__':
+  
+    app.run(host="0.0.0.0")
