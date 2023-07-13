ee2f9a742433f0acb504569736d4e2036c9b13b7
Shivkumar Gaded <gadedshiv@gmail.com>
2023-07-13 17:43:06 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,14 @@
+import json
 from flask import Flask, jsonify, request
-  
+
 # creating a Flask app
 app = Flask(__name__)
 
 @app.route('/', methods = ['POST'])
 def home():
-    given_name = request.json['given_name']
-    surname = request.json['surname']
-    result = '{cutomers : [{"given_name": "Michael","surname": "Jack son","street_number": "MG Road","address_1": "","address_2": "Pune","suburb": "","postcode": 411045,"state": "MH","mobile": "9756656656","email": "abc@gmail.com","account": "6545646546564","IBAN": "HDFC566565","confidence":0.99765},{"given_name": "Michael","surname": "Jack son","street_number": "","address_1": "","address_2": "","suburb": "","postcode": 411045,"state": "MH","mobile": "9756656656","email": "xyz@gmail.com","account": "56445645646","IBAN": "HDFC26155","confidence":0.98765}]}'
-    return jsonify(result)
+    result = '{"customers" : [{"given_name": "Michael","surname": "Jack son","street_number": "MG Road","address_1": "","address_2": "Pune","suburb": "","postcode": 411045,"state": "MH","mobile": "9756656656","email": "abc@gmail.com","bankname":"HDFC","account": "6545646546564","IBAN": "HDFC566565","confidence":0.99765},{"given_name": "Michael","surname": "Jack son","street_number": "","address_1": "","address_2": "","suburb": "","postcode": 411045,"state": "MH","mobile": "9756656656","email": "xyz@gmail.com","bankname":"HDFC","account": "56445645646","IBAN": "HDFC26155","confidence":0.98765}]}'
+    y = json.loads(result)
+    return jsonify(y["customers"][0])
 
 if __name__ == '__main__':
   
